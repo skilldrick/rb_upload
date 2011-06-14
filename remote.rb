4d82271e241d77f5a3929a02e677eca609b9eb1d
skilldrick <skilldrick@gmail.com>
2011-06-14 18:57:42 +0100

    Uploads text and binary files now.

--- remote.rb
+++ remote.rb
@@ -2,6 +2,7 @@ require 'net/ftp'
 
 class Remote
   attr_accessor :connected
+  @@ascii_extensions = ['txt',  'php', 'html']
 
   def connect(credentials)
     @ftp = Net::FTP.new(credentials.machine)
@@ -14,6 +15,15 @@ class Remote
   end
 
   def upload(from_path, to_path)
+    if ascii? from_path
+      @ftp.puttextfile(from_path, to_path)
+    else
+      @ftp.putbinaryfile(from_path, to_path)
+    end
+  end
+
+  def ascii? file_path
+    file_path.end_with?(*@@ascii_extensions)
   end
 
 end
--- remote_spec.rb
+++ remote_spec.rb
@@ -2,23 +2,33 @@ require 'remote'
 require 'settings'
 
 describe Remote do
+  def connect
+    settings = Settings.new
+    credentials = settings.get_credentials_from_site 'development'
+    @remote.connect credentials
+  end
+
   before(:each) do
     @remote = Remote.new
   end
 
   it "should connect with correct credentials" do
-    settings = Settings.new
-    credentials = settings.get_credentials_from_site 'development'
-    @remote.connect credentials
+    connect
     @remote.list #force it to do something
   end
 
-  it "should upload file" do
-    settings = Settings.new
-    credentials = settings.get_credentials_from_site 'development'
-    @remote.connect credentials
+  it "should upload ascii file" do
+    connect
+    @remote.upload('test_files/file1.txt', '/public_html/upload_testing/file1.txt')
   end
 
+  it "should upload binary file" do
+    connect
+    @remote.upload('test_files/file3.jpg', '/public_html/upload_testing/file3.jpg')
+  end
 
+  it "should distinguish ascii from binary files" do
+    @remote.ascii?('test/text.txt').should == true
+    @remote.ascii?('test/image.jpg').should == false
+  end
 end
-
