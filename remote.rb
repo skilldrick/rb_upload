50a15fe70398f5a91067d5891fa06989393f7f4e
skilldrick <skilldrick@gmail.com>
2011-06-14 18:32:50 +0100

    Improved ftp code.

--- remote.rb
+++ remote.rb
@@ -4,9 +4,16 @@ class Remote
   attr_accessor :connected
 
   def connect(credentials)
-    ftp = Net::FTP.new(credentials.machine)
-    ftp.passive = true
-    ftp.login(credentials.login, credentials.password)
-    ftp.list #force it to do something
+    @ftp = Net::FTP.new(credentials.machine)
+    @ftp.passive = true
+    @ftp.login(credentials.login, credentials.password)
   end
+
+  def list
+    @ftp.list
+  end
+
+  def upload(from_path, to_path)
+  end
+
 end
--- remote_spec.rb
+++ remote_spec.rb
@@ -10,6 +10,15 @@ describe Remote do
     settings = Settings.new
     credentials = settings.get_credentials_from_site 'development'
     @remote.connect credentials
+    @remote.list #force it to do something
   end
+
+  it "should upload file" do
+    settings = Settings.new
+    credentials = settings.get_credentials_from_site 'development'
+    @remote.connect credentials
+  end
+
+
 end
 
