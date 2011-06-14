1e87b13abcc832c7f72b77cb0af26c4fd2f1681c
skilldrick <skilldrick@gmail.com>
2011-06-14 14:58:42 +0100

    Local files now ignores prefixes and suffixes

--- local_files.rb
+++ local_files.rb
@@ -1,5 +1,22 @@
 class LocalFiles
-  def get_files directory
-    ['a', 'b', 'c']
+  attr_accessor :ignored_prefixes, :ignored_suffixes
+
+  def initialize ignored_prefixes, ignored_suffixes, directory
+    @ignored_prefixes = ignored_prefixes
+    @ignored_suffixes = ignored_suffixes
+    @directory = directory
+    @ignored_prefixes.map! { |prefix| File.join(@directory, prefix) }
+  end
+
+  def ignore? file_path
+    file_path.start_with?(*@ignored_prefixes) || file_path.end_with?(*@ignored_suffixes)
+  end
+
+  def get_files
+    files = Dir["#{@directory}/**/*.*"] #get all files recursively
+    files = files.reject do |file|
+      ignore? file
+    end
+    files
   end
 end
--- local_files_spec.rb
+++ local_files_spec.rb
@@ -2,12 +2,29 @@ require 'local_files'
 
 describe LocalFiles do
   before(:each) do
-    @local_files = LocalFiles.new
+    @local_files = LocalFiles.new ['ignored_dir'], ['.swp'], 'test_files'
   end
 
   it "should load a list of files in specified directory" do
-    files = @local_files.get_files 'test_files'
+    files = @local_files.get_files
     files.count.should have_at_least(1).items
+  end
+
+  it "should ignore ignored prefixes" do
+    @local_files.ignore?('test_files/ignored_dir/testfile.txt').should == true
+    @local_files.ignore?('test_files/not_ignored_dir/testfile.txt').should == false
+  end
+
+  it "should ignore ignored prefxes" do
+    files = @local_files.get_files
+    files.should_not include 'test_files/ignored_dir/file6.txt'
+    files.should include 'test_files/dir1/file4.txt'
+  end
+
+  it "should ignore ignored suffixes" do
+    files = @local_files.get_files
+    files.should_not include 'test_files/file.swp'
+    files.should include 'test_files/file.swp.txt'
   end
 end
 
